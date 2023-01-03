330fc5228b9e59550e5e4c886d597a13b11c6765
junkei_okinawa <ceazy.x2.okinawan@gmail.com>
2023-01-03 16:43:26 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../ui/Link';
+import ProTip from '../ui/components/ProTip';
+import Copyright from '../ui/components/Copyright';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <Link href="/about" color="secondary">
+          Go to the hello page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
